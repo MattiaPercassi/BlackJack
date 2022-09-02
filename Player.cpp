1b8717b981b285539075813053e1113e4f596c22
Mattia Percassi <107413452+MattiaPercassi@users.noreply.github.com>
2022-09-02 13:27:21 +0900

    Implemented basic checkScore method in Player, will not compile, needed check for std::count utilization

--- Player.cpp
+++ Player.cpp
@@ -7,24 +7,33 @@
 #include <algorithm>
 #include <numeric>
 
-virtual int Player::checkScore()
+int Player::checkScore()
 {
-    int tempScore {0};
+    int tempScore{0};
     // No Ace score, sum all cards without Aces (lambda function dependent on the value assigned to ace)
-    //TODO - checkValue should return the value between 1 and 10, for the actual card symbol we need an extra member to be shown
-    int acesCount = std::count(hand.begin(), hand.end(), [](Card &c) {return (c.checkValue()==1);});
-    tempScore += std::accumulate(hand.begin(), hand.end(), [](Card &c) {return (c.checkValue()!=1);});
-    // if we are above 21 or we already checked all cards then the tempScore will be also the final one and should be returned
-    if (tempScore > 21 || acesCount == 0 ) return tempScore;
-    // if adding only aces counted as 1 we get 21 or higher, as this would be the minimum score we return the result
-    if (tempScore + acesCount >= 21) return tempScore + acesCount;
-    // if instead we do not reach 21 we have to check what happens when we consider aces as 11 and not 1 --> we start from the highest score so we will return only if we get below 21, if not after the for loop we will return the score calculated with minimum aceCount
-    for (int i {acesCount}; i>=1; --i)
+    // TODO - checkValue should return the value between 1 and 10, for the actual card symbol we need an extra member to be shown
+    int acesCount = std::count(hand.begin(), hand.end(), [](Card &c)
+                               { return (c.checkValue() == 1); });
+    for (auto &card : hand)
     {
-        score += i*11 + acesCount - i;
-        //since we start from the highest score, if this is less or equal to 21 wit is final
-        if (score <= 21) return score;
+        if (card.checkValue() != 1)
+            tempScore += card.checkValue();
+    };
+    // if we are above 21 or we already checked all cards then the tempScore will be also the final one and should be returned
+    if (tempScore > 21 || acesCount == 0)
+        return tempScore;
+    // if adding only aces counted as 1 we get 21 or higher, as this would be the minimum score we return the result
+    if (tempScore + acesCount >= 21)
+        return tempScore + acesCount;
+    // if instead we do not reach 21 we have to check what happens when we consider aces as 11 and not 1 --> we start from the highest score so we will return only if we get below 21, if not after the for loop we will return the score calculated with minimum aceCount
+    int score{tempScore};
+    for (int i{acesCount}; i >= 1; --i)
+    {
+        score += i * 11 + acesCount - i;
+        // since we start from the highest score, if this is less or equal to 21 wit is final
+        if (score <= 21)
+            return score;
     }
-    //in case no combination was reached that could generate a score <=21
+    // in case no combination was reached that could generate a score <=21
     return tempScore + acesCount;
 };
